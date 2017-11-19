837b48704700c1339b99c8d8c61cb0bb6f310395
Evan Genest <genest.1@osu.edu>
2017-11-19 10:35:56 -0600

    First commit.   This is the file where all code should be written.

--- js/request-logic.js
+++ js/request-logic.js
@@ -2,6 +2,8 @@
 // extension off of the supplied filename, but we can't figure out the rest of
 // the function to use it! We hope this is useful to you!
 
+/*      ALL     WORK     IS     WRITTEN   IN     HERE  
+*/
 
 function getContentType(filename) {
   const extension = filename.match(/.*\.([^\.]*)$/)[1];
